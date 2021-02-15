4044b234e9e466093edbd6cb8f8f2afdc9ad00f5
Guillaume Rager <grager@vinotekasoft.com>
2021-02-15 13:18:21 +0100

    Update appmacros.h

--- InAppStore/appmacros.h
+++ InAppStore/appmacros.h
@@ -5,10 +5,5 @@
 
 #import <Foundation/Foundation.h>
 
-#ifdef _VINOTEKA4_VERSION_
 #define __BUNDLE_ID__       "com.vinotekasoft.Vinoteka"
 #define __BUNDLE_VERSION__  "4.0.0"
-#else
-#define __BUNDLE_ID__       "com.vinotekasoft.Vinoteka"
-#define __BUNDLE_VERSION__  "3.6.4"
-#endif
